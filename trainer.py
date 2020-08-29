22ca94b97cd4a469a818967ff88e0785a4886074
Satadru Hazra <58564580+Satadru-coder@users.noreply.github.com>
2020-08-29 20:47:05 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -6,7 +6,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
     tf.keras.layers.Dense(24, activation='relu'),
-    tf.keras.layers.Dense(6, activation='sigmoid')
+    tf.keras.layers.Dense(6, activation='softmax')
 ])
 
 model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
